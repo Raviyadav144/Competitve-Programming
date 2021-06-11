1a3a35f6bb4b421a77df6c57c75a4e35f0b9f066
Ravi Singh Yadav <raviisyadav13@gmail.com>
2021-06-12 03:41:50 +0530

    trying

---
+++ rou.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define forn(i,j,n) for(int i =j; i<n; i++)
+#define ll long long
+#define all(v)  v.begin() , v.end()
+#define vi(v,n) vector<int> v(n)
+#define str string
+#define fora(e,v) for(auto &e :v)
+
+
+int main() 
+{	
+	
+	int a = 816212230;
+	cout << (a%50)%50;
+	
+}
