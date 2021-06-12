21fd8cc554c625ad0c729721190b7c2a1e9ef092
Ravi Singh Yadav <raviisyadav13@gmail.com>
2021-06-13 01:57:56 +0530

    to find number of divisors

---
+++ no_of_divisors.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define forn(i,j,n) for(int i =j; i<n; i++)
+#define ll long long
+#define all(v)  v.begin() , v.end()
+#define vi(v,n) vector<int> v(n)
+#define str string
+#define fora(e,v) for(auto &e :v)
+
+
+
+
+int main() 
+{	
+	int n;
+	cin >>n;
+	int count=0;
+	for(int i=1;i*i<=n;i++)
+	{
+		if(!(n%i))
+		{	count++;
+			int otherDivisor = n/i;
+			if(otherDivisor!=i)
+				count++;
+		}
+	}
+	cout << count << endl;
+}
