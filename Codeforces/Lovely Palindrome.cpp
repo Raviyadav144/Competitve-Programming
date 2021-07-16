17efeb4b40da500432e07cfb14b5e7292bc5bd12
Ravi Singh Yadav <raviisyadav13@gmail.com>
2021-07-17 02:28:34 +0530

    CF 360 B

---
+++ Codeforces/Lovely Palindrome.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define forn(i,j,n) for(int i =j; i<n; i++)
+#define ll long long
+#define all(v)  v.begin() , v.end()
+#define vi(v,n) vector<int> v(n)
+//#define str string
+#define fora(e,v) for(auto &e :v)
+#define endl "\n"
+#define pi acos(-1.0)
+
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    string pali;
+    cin >> pali;
+    cout << pali;
+    for(int i = pali.size()-1;i>=0;i--)
+        cout << pali[i];
+}
