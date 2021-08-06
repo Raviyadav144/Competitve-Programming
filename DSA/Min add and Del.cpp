8a105d908b5605e5f9ae627cb419c7d6984d32f3
Ravi Singh Yadav <raviisyadav13@gmail.com>
2021-08-06 12:25:37 +0530

    Minimum number od Insertion and Deletion to make two strings equal

---
+++ DSA/Min add and Del.cpp
@@ -0,0 +1,45 @@
+//-----raviyadav144----//
+#include<bits/stdc++.h>
+using namespace std;
+
+#define forn(i,j,n) for(int i =j; i<n; i++)
+#define ll long long
+#define all(v)  v.begin() , v.end()
+#define vi(v,n) vector<int> v(n)
+#define fora(e,v) for(auto &e :v)
+#define endl "\n"
+#define pi acos(-1.0)
+#define mod 1000000009
+
+
+int MinInDel(string &x , string &y , int n , int m)
+{
+    int dp[n+1][m+1];
+    memset(dp,0,sizeof(dp));
+    for(int i =1;i<=n;i++)
+    {
+        for(int j = 1;j<=m;++j)
+        {
+            if(x[i-1]==y[j-1])
+                dp[i][j] = 1+dp[i-1][j-1];
+            else
+                dp[i][j] = max(dp[i-1][j],dp[i][j-1]);
+        }
+    }
+    int ans = dp[n][m];
+    return (m-ans + n- ans);
+}
+
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    string x , y;
+    cin >> x >> y;
+    int n = x.length();
+    int m = y.length();
+    cout << MinInDel(x,y,n,m) << endl;
+
+}
+
