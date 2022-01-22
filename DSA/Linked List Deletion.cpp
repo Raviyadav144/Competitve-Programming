ed0120faa938280fbe840077a013767f6ac899b0
raviyadav144 <raviisyadav13@gmail.com>
2022-01-23 02:24:10 +0530

    Linked list GFG

---
+++ DSA/Linked List Deletion.cpp
@@ -0,0 +1,115 @@
+//-----raviyadav144----//
+#include<bits/stdc++.h>
+using namespace std;
+
+#define forn(i,j,n) for(int i =j; i<n; i++)
+#define ll long long
+#define all(v)  v.begin() , v.end()
+#define vi(v,n) vector<int> v(n)
+#define fora(e,v) for(auto &e :v)
+#define endl "\n"
+#define pi acos(-1.0)
+#define mod 1000000007
+
+
+
+
+/* Link list Node */
+struct Node
+{
+    int data;
+    struct Node* next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+
+void append(struct Node** head_ref, struct Node **tail_ref,
+           int new_data)
+{
+    struct Node* new_node = new Node(new_data);
+    
+    if (*head_ref == NULL)
+       *head_ref = new_node;
+    else
+       (*tail_ref)->next = new_node;
+    *tail_ref = new_node;
+}
+
+/* Function to get the middle of the linked list*/
+struct Node* deleteNode(struct Node *head,int );
+
+void printList(Node *head)
+{
+   while (head != NULL)
+   {
+      cout << head->data << " ";
+      head = head->next;
+   }
+   cout << "\n";
+}
+
+/* Driver program to test above function*/
+int main()
+{
+  int T, i, n, l;
+
+    cin>>T;
+
+    while(T--){
+    struct Node *head = NULL,  *tail = NULL;
+
+        cin>>n;
+        for(i=1;i<=n;i++)
+        {
+            cin>>l;
+            append(&head, &tail, l);
+        }
+
+    int kk;
+    cin>>kk;
+    head = deleteNode(head,kk);
+    printList(head);
+    }
+    return 0;
+}
+// } Driver Code Ends
+
+
+/* Link list Node 
+struct Node
+{
+    int data;
+    struct Node* next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+*/
+
+/*You are required to complete below method*/
+Node* deleteNode(Node *head,int x)
+{
+    //Your code here
+    if(x==1)
+    {
+        head = head -> next;
+    }
+    else
+    {
+        Node * temp  = head;
+        int count = 1;
+        while(count != (x-1))
+        {
+            temp = temp -> next;
+            count++;
+        }
+        temp -> next = temp -> next ->next;
+         
+    }
+    return head;
+}
