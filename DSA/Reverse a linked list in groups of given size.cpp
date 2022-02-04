54b930fec175bb37e6377dce74a74f68882fb035
raviyadav144 <raviisyadav13@gmail.com>
2022-02-04 19:41:08 +0530

    Amazon Medium

---
+++ DSA/Reverse a linked list in groups of given size.cpp
@@ -0,0 +1,148 @@
+//-----raviyadav144----//
+#include<bits/stdc++.h>
+using namespace std;
+
+#define forn(i,j,n) for(int i =j; i<n; i++)
+#define ll long long
+#define all(v)  v.begin() , v.end()
+#define vi(v,n) vector<int> v(n)
+#define fora(e,v) for(auto &e :v)
+#define endl "\n"
+#define pi acos(-1.0)
+#define mod 1000000007
+
+
+
+struct node
+{
+    int data;
+    struct node* next;
+    
+    node(int x){
+        data = x;
+        next = NULL;
+    }
+    
+};
+
+/* Function to print linked list */
+void printList(struct node *node)
+{
+    while (node != NULL)
+    {
+        printf("%d ", node->data);
+        node = node->next;
+    }
+    printf("\n");
+}
+
+
+ // } Driver Code Ends
+/*
+  Reverse a linked list
+  The input list will have at least one element  
+  Return the node which points to the head of the new LinkedList
+  Node is defined as 
+    struct node
+    {
+        int data;
+        struct node* next;
+    
+        node(int x){
+            data = x;
+            next = NULL;
+        }
+    
+    }*head;
+*/
+
+class Solution
+{
+    public:
+    struct node *reverse (struct node *head, int k)
+    { 
+        // Complete this method
+        node* temp = head;
+        node* it = head;
+        int count = 0;
+        stack<int>st;
+        if(k==1)
+            return head;
+        while(count <= k && temp  != NULL)
+        {
+            if(count==k)
+            {
+                while(!st.empty())
+                {
+                    it -> data = st.top();
+                    st.pop();
+                    it = it -> next;
+                }
+                count =0;
+                
+            }
+            
+            st.push(temp -> data);
+            temp = temp -> next;
+            count++;
+            
+        }
+        if(count!=0)
+        {   
+            cout << count <<endl;
+            
+            while(!st.empty())
+            {
+                it -> data = st.top();
+                st.pop();
+                it = it -> next;
+            }
+        }
+        return head;
+    }
+};
+
+
+// { Driver Code Starts.
+
+/* Drier program to test above function*/
+int main(void)
+{
+    int t;
+    cin>>t;
+     
+    while(t--)
+    {
+        struct node* head = NULL;
+        struct node* temp = NULL;
+        int n;
+        cin >> n;
+         
+        for(int i=0 ; i<n ; i++)
+        {
+            int value;
+            cin >> value;
+            if(i == 0)
+            {
+                head = new node(value);
+                temp = head;
+            }
+            else
+            {
+                temp->next = new node(value);
+                temp = temp->next;
+            }
+        }
+        
+        int k;
+        cin>>k;
+        
+        Solution ob;
+        head = ob.reverse(head, k);
+        printList(head);
+    }
+     
+    return(0);
+}
+
+  // } Driver Code Ends
