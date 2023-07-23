c34cd08919cb75b04e8c4322da54c451d30f81e3
raviyadav144 <raviisyadav13@gmail.com>
2023-07-23 12:36:57 +0530

    Using Stack or vector

---
+++ Leetcode/Merge Intervals.cpp
@@ -0,0 +1,58 @@
+//-----raviyadav144----//
+#include<bits/stdc++.h>
+using namespace std;
+
+#define forn(i,j,n) for(int i =j; i<n; i++)
+#define ll long long
+#define all(v)  v.begin() , v.end()
+#define vi(v,n) vector<int> v(n)
+#define fora(e,v) for(auto &e :v)
+#define endl "\n"
+#define pi acos(-1.0)
+#define mod 1000000007
+
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        sort(intervals.begin(), intervals.end());
+        int n = intervals.size();
+        vector<vector<int>>v;
+        v.push_back(intervals[0]);
+        for(int i =1 ; i < n;++i)
+        {
+            if(intervals[i][0] <= v.back()[1])
+                v.back()[1] = max(v.back()[1], intervals[i][1]);
+            else
+                v.push_back(intervals[i]);
+        }
+        return v;
+
+    }
+};
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    // vector<int>v;
+    // int n;
+    // cin>> n;
+
+
+    Solution sol;
+    // vector<int> ans  = sol.getRow(n);
+    vector<vector<int>>v = {{1,3}, {2,6},{8,10},{15,18}};
+    sol.merge(v);
+    fora(e , v)
+    {
+        fora(a ,e)
+        {
+            cout << a<< " ";
+        }
+        cout << endl;
+        // cout << e << endl;
+    }
+    // cout << ans << endl;
+    // // cout << endl;
+   
+}
